1c576b52b86b7b1d2797bccf06fc39e34f95fbc5
introbond <introbond.dev@gmail.com>
2022-10-26 13:21:07 +0700

    Initialize

---
+++ readData.js
@@ -0,0 +1,25 @@
+const ss = SpreadsheetApp.openById('xxx')
+const sheet = ss.getSheetByName('xxx')
+
+function doGet(e) {
+    let action = e.parameter.action
+    if (action == 'getUsers') {
+        return readData(e)
+      }
+}
+
+function readData(e) {
+    let rows = sheet.getRange(2,1,sheet.getLastRow()-1,sheet.getLastColumn()).getValues()
+    let data =[]
+    let row = 0
+    let record = {}
+    for(let i = 0; i < rows.length; i++){
+        row = rows[i]
+        record['no']=row[0]
+        record['name']=row[1]
+        record['description']=row[2]
+        data.push(record)
+    }
+     let result = JSON.stringify(data)
+    return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.JSON)
+}
---
+++ writeData.js
@@ -0,0 +1,19 @@
+var ss = SpreadsheetApp.openById('xxx')
+var sheet = ss.getSheetByName('xxx')
+
+function doPost(e) {
+    var action = e.parameter.action
+    if (action == 'writeData') {
+        return writeData(e)
+    }
+}
+
+function writeData(e) {
+    let data = JSON.parse(e.postData.contents)
+    sheet.appendRow([
+        data.no,
+        data.name,
+        data.description
+    ])
+  return ContentService.createTextOutput("success").setMimeType(ContentService.MimeType.TEXT)
+}
